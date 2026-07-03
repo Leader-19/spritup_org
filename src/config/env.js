65e9969a6f4e7ea46d81b6f994ed10bb38e7b34d
Leader-19 <dinleader200@gmail.com>
2026-07-03 21:34:48 +0700

    add new

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,2 @@
-export const API_URL = 'http://16.176.28.96'
-export const API_BASE = 'http://16.176.28.96/api'
-
-// http://16.176.28.96/
+export const API_URL = 'https://movie-ticket-booking.online'
+export const API_BASE = 'https://movie-ticket-booking.online/api'
